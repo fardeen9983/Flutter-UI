3565eb84e337da32d0642313b39607625381f1b1
Fardeen Khan <khanfardeen9983@gmail.com>
2019-10-11 18:04:14 +0530

    DespicableMe: Initial file structure

---
+++ despicable_me/lib/ui/pages/character_list_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CharacterListPage extends StatefulWidget {
+  @override
+  _CharacterListPageState createState() => _CharacterListPageState();
+}
+
+class _CharacterListPageState extends State<CharacterListPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
